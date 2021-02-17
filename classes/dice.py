9e961283f31bbbee488a4e33174f593e7378c840
kevinniel <k.niel.pro@gmail.com>
2021-02-17 12:28:05 +0100

    add comments

--- classes/dice.py
+++ classes/dice.py
@@ -1,23 +1,62 @@
 from random import randint
 
 class Dice:
+    """
+    Création d'une classe qui gère un objet "Dé"
+    """
 
     def __init__(self, nb_face = 6):
+        """
+        Constructeur de la classe. Le constructeur est une méthode
+        qui est automatiqument déclenchée lors de la création
+        d'un objet
+
+        Le paramètre "nb_face" est mis à "6" par défaut, et il peut
+        être modifié lors de la création d'un objet en passant un
+        nombre entre les parenthèses (par exemple : Dices(7) )
+        """
+
+        # Définition d'un attribut "nb_face"
         self.nb_face = nb_face
+
+        # Définition d'un attribut "current_value" prennant pour
+        # valeur par défaut "0".
         self.current_value = 0
 
     def roll(self):
+        """
+        Méthode permettant de lancer le dé. Elle définit la valeur
+        courante du dé (self.current_value) à un nouveau nombre 
+        aléatoire entre 1 et nb_face
+        """
         self.current_value = randint(1, self.nb_face)
 
     def pick_a_number(self, choosen_number):
+        """
+        Méthode permettant de définir une valeur courante pour le dé
+        """
         self.current_value = choosen_number
 
     def __add__(self, other):
+        """
+        Méthode spéciale permettant à python de savoir additionner
+        2 objets de type "Dice"
+        """
         return self.current_value + other.current_value
 
     def __eq__(self, other):
+        """
+        Méthode spéciale permettant à python de pouvoir comparer
+        2 objets de type "Dice" avec le symbole " == "
+        """
         return self.current_value == other.current_value
 
+# La condition ci-dessous permet de n'exécuter un code QUE lorsque
+# le fichier python est exécuté directement.
+# Si vous instanciez ce fichier en exécutant la commande "python dice.py"
+# alors ce qui est dedans s'exécutera.
+# Par contre si ce fichier est appelé dans un autre (par exemple dans 
+# dices.py), alors le contenu de ce bloc "if" ne sera pas exécuté.
 if __name__ == "__main__":
     dice1 = Dice()
     dice2 = Dice()
--- classes/dices.py
+++ classes/dices.py
@@ -1,21 +1,42 @@
+# Import de la classe Dice depuis le fichier dice.py
 from dice import Dice
 
 class Dices:
+    """
+    Création d'une classe gérant 2 dés
+    """
 
     def __init__(self):
+        """
+        Constructeur. Aucun paramètre nécessaire
+        """
+
+        # création d'un attribut "dice1" représentant 1 dé
         self.dice1 = Dice()
+        # création d'un attribut "dice1" représentant 1 autre dé
         self.dice2 = Dice()
+        # création d'un attribut représentant la somme des 2 dés
         self.sum_dices = 0
 
     def roll(self):
+        """
+        Méthode permettant de lancer tous les dés.
+        """
         self.dice1.roll()
         self.dice2.roll()
+        # mise à jour de la somme des dés une fois lancés
         self.sum_dices = self.get_sum()
 
     def get_sum(self):
+        """
+        Méthode permettant de récupérer la valeur de la somme des dés.
+        """
         return self.dice1 + self.dice2
 
     def is_double(self):
+        """
+        Méthode permettant de savoir si un double a été effectué ou non.
+        """
         return self.dice1 == self.dice2
 
 if __name__ == "__main__":
