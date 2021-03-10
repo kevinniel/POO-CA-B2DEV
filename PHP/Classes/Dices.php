a24a6c78ca52619ba1d32ba91ebe4a3a4a610391
kevinniel <k.niel.pro@gmail.com>
2021-03-10 09:41:01 +0100

    update

---
+++ PHP/Classes/Dices.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once("Dice.php");
+
+class Dices
+{
+    public $dice1;
+    public $dice2;
+    public $sumDices;
+    public $isDouble;
+
+    public function __construct()
+    {
+        $this->dice1 = new Dice();
+        $this->dice2 = new Dice();
+    }
+
+    public function roll()
+    {
+        $this->dice1.roll();
+        $this->dice2.roll();
+        $this->updateSum();
+        $this->updateDouble();
+    }
+
+    private function updateDouble()
+    {
+        $this->isDouble = $this->isDouble();
+    }
+
+    private function isDouble()
+    {
+        return $this->dice1->currentValue == $this->dice2->currentValue;
+    }
+
+    private function updateSum()
+    {
+        $this->sumDices = $this->calculateSum();
+    }
+
+    private function calculateSum()
+    {
+        return $this->dice1->currentValue + $this->dice2->currentValue;
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Cours POO B2 DEV - CA
+
+- 1er jour => Python
+
+- 2eme jour => PHP
